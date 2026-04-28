cf92220464e646a11226443ae40502fbeeeb50db
16Nov16 <cegavinspam@gmail.com>
2026-04-28 16:16:29 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ 
+      fontFamily: 'sans-serif', 
+      maxWidth: '600px', 
+      margin: '0 auto', 
+      padding: '20px' 
+    }}>
+      <h1>HTGYST</h1>
+      <p>How To Get Your Shit Together</p>
+      <p>Decision Log coming soon.</p>
+    </div>
+  )
+}
+
+export default App
